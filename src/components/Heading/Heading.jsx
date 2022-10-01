e5ba32473bb48d7b4a00041580121848f24c2e23
Sayam <sayamalvi07@gmail.com>
2022-10-01 13:42:28 +0530

    entry animation done

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Heading.scss'
-
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 const Heading = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000 })
+    }, [])
     return (
-        <div className='heading'>
+        <div className='heading' data-aos="fade-up">
             <h4>WELCOME TO</h4>
             <h1 className='adc'>ALVI DENTAL CARE </h1>
             <p>A one stop solution for all your dental needs.</p>
