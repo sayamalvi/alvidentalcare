0114d6e38b4c51b1f42646461bdb9404fc7b1b93
Sayam <sayamalvi07@gmail.com>
2023-01-04 19:29:58 +0530

    updated gallery component

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from "../Navbar/Nav"
 import images from './images'
 import classes from "./Gallery.module.scss"
 const Gallery = () => {
+    // const [selectedImage, setSelectedImage] = useState(null);
+    // const handleClick = (image) => {
+    //     setSelectedImage(image)
+    // }
+    // const handleClose = () => {
+    //     setSelectedImage(null)
+    // }
     return (
         <>
             <Nav />
             <div className={classes.gallery}>
-                {images.map((image, index) => {
-                    return (
-                        <div className={classes.gallery__pics} key={index}>
-                            <img src={image.imgSrc} alt='alvi dental care'></img>
+                {/* {selectedImage && (
+                    <div className={classes.gallery_selected_image_overlay} >
+                        Close
+                    </div>
+                )} */}
+                <div className={classes.gallery_image_grid}>
+                    {images.map((image, index) => (
+                        <div className={classes.gallery_image_cover}>
+                            <img src={image.imgSrc} alt='alvi dental care'
+                                className={classes.gallery_image}
+                                key={index}
+                            />
                         </div>
-                    )
-                })}
+                    ))}
+                </div>
             </div>
         </>
 
@@ -21,3 +36,5 @@ const Gallery = () => {
 }
 
 export default Gallery
+
+
